095d542322462dfb4f7147ac2a6109e271b96654
bh6aol <64899471+bh6aol@users.noreply.github.com>
2020-05-06 18:38:20 +0800

    Add files via upload

---
+++ pic/auto.sh
@@ -0,0 +1,8 @@
+ffmpeg -i qScreenshot_2020-05-06-09-48-56.png  -vf "scale=300:300/a" Screenshot_2020-05-06-09-48-56.png
+ffmpeg -i qScreenshot_2020-05-06-09-49-22.png  -vf "scale=300:300/a" Screenshot_2020-05-06-09-49-22.png
+ffmpeg -i qScreenshot_2020-05-06-09-49-25.png  -vf "scale=300:300/a" Screenshot_2020-05-06-09-49-25.png
+ffmpeg -i qScreenshot_2020-05-06-09-50-32.png  -vf "scale=300:300/a" Screenshot_2020-05-06-09-50-32.png
+ffmpeg -i qScreenshot_2020-05-06-09-59-47.png  -vf "scale=300:300/a" Screenshot_2020-05-06-09-59-47.png
+ffmpeg -i qScreenshot_2020-05-06-10-03-55.png  -vf "scale=300:300/a" Screenshot_2020-05-06-10-03-55.png
+ffmpeg -i qwx.png  -vf "scale=300:300/a" wx.png
+ffmpeg -i qzfb.png  -vf "scale=300:300/a" zfb.png
